de4096dcd0304055e9e0811bfd6e17998d0c4bca
Jason Yapri <jason.yapri@gmail.com>
2024-10-04 00:33:09 +0700

    Changed base sepolia contract address

--- src/app/constants/contracts.ts
+++ src/app/constants/contracts.ts
@@ -1,2 +1,2 @@
 export const COUNTER = "0x4E55215575606D4A57e165a9169FD651F520B99b";
-export const PBACERT = "0x085a764BDe4C7EcD68873d4F639CA62d48c31aea"; // Base Sepolia
+export const PBACERT = "0x486883E79A1b5d3eA50f4d7e5Dd9Eb8c375E0e9b"; // Base Sepolia
