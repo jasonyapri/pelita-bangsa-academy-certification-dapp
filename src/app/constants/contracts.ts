a8524f01fe5fae3adaca4d00981c940f288adab2
Jason Yapri <jason.yapri@gmail.com>
2024-10-04 13:27:33 +0700

    Move to mainnet

--- src/app/constants/contracts.ts
+++ src/app/constants/contracts.ts
@@ -1,2 +1,3 @@
 export const COUNTER = "0x4E55215575606D4A57e165a9169FD651F520B99b";
-export const PBACERT = "0x486883E79A1b5d3eA50f4d7e5Dd9Eb8c375E0e9b"; // Base Sepolia
+// export const PBACERT = "0x486883E79A1b5d3eA50f4d7e5Dd9Eb8c375E0e9b"; // Base Sepolia
+export const PBACERT = "0xd25F0F6E2a9c1960d19CF41a5fcd20684E9dC759"; // Base Mainnet
